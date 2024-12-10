954e9cfa36b1fceb106396bd202a6b302c0202d2
Artur Nõmmik <artur.nymmik@gmail.com>
2024-12-11 01:49:37 +0200

    Fixed ConfigHandling uncommon behaviour

--- ConfigHandling.py
+++ ConfigHandling.py
@@ -4,8 +4,7 @@ import os
 
 def initialize_config(config_file="configHandling.csv"):
     if not os.path.exists(config_file):
-        data = {
-        }
+        data = {}
 
         df = pd.DataFrame(data)
         df.to_csv(config_file, index=False)
@@ -14,22 +13,22 @@ def initialize_config(config_file="configHandling.csv"):
 
 def read_config(config_file="configHandling.csv"):
     df = pd.read_csv(config_file)
-    config = [df.iat[0, 0], df.iat[1, 0]]
+    config = [df.iat[0, 1], df.iat[1, 1]]
     print(config)
 
     return config[0], config[1]
 
 
 def write_config(noise_folder, last_used_folder, config_file="configHandling.csv"):
+    print(f"{noise_folder}/{last_used_folder}")
     df = pd.read_csv(config_file)
 
     if noise_folder is not None:
-        df.iat[0, 0] = noise_folder
+        df.iat[0, 1] = noise_folder
+        df.loc[df['parameter'] == 'noise_folder', 'value'] = str(noise_folder)
     if last_used_folder is not None:
-        df.iat[1, 0] = last_used_folder
-
-    df.loc[df['parameter'] == 'noise_folder', 'value'] = str(noise_folder)
-    df.loc[df['parameter'] == 'last_used_folder', 'value'] = str(last_used_folder)
+        df.iat[1, 1] = last_used_folder
+        df.loc[df['parameter'] == 'last_used_folder', 'value'] = str(last_used_folder)
 
     df.to_csv(config_file, index=False)
     print(f"Config updated: noise_folder={noise_folder}, last_used_folder={last_used_folder}")
